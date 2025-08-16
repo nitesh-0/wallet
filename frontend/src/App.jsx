96a0de0cd313cef026ab0b42ffa407009b5af7f4
nitesh-0 <niteshsah760@gmail.com>
2025-08-16 15:28:36 +0545

    some routing fix on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Dashboard } from './pages/Dashboard'
@@ -7,22 +7,27 @@ import { Send } from './pages/Send'
 import { PrivateRouter } from './components/PrivateRouter'
 import { Landing } from './pages/Landing'
 
-
 function App() {
-
   return (
-    <div >
-      <BrowserRouter>
-      <PrivateRouter/>
+    <BrowserRouter>
       <Routes>
-        <Route path = "/signup" element = {<Signup/>} />
-        <Route path = "/signin" element = {<Signin/>} />
-        <Route path = "/dashboard" element = { <PrivateRouter><Dashboard/></PrivateRouter>  } />
-        <Route path="/send" element = { <PrivateRouter><Send/></PrivateRouter> } />
-        <Route path="/" element = {<Landing/>} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        
+        <Route path="/dashboard" element={
+          <PrivateRouter>
+            <Dashboard />
+          </PrivateRouter>
+        } />
+        
+        <Route path="/send" element={
+          <PrivateRouter>
+            <Send />
+          </PrivateRouter>
+        } />
       </Routes>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
   )
 }
 
