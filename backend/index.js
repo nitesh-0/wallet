032a3fed3ede9647c60a8dc5c22409c35cfe2ea4
nitesh-0 <niteshsah760@gmail.com>
2025-08-16 15:05:48 +0545

    some backend fixes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.use("/api/v1", rootRouter)
 
-
-app.listen(3000, () => {
-    console.log("listening on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
